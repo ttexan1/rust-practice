bd3fc190c16bb9bfe30dbdf341dd3a509c45bcb1
ttexan1 <tetsuji.higashi@geniee.co.jp>
2021-09-06 10:40:59 +0900

    made guess game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+// use ferris_says::say; // from the previous step
+// use std::io::{stdout, BufWriter};
+
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello fellow Rustaceans!");
+//     let width = message.chars().count();
+
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(message.as_bytes(), width, &mut writer).unwrap();
+// }
+
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {}", secret_number); // this is the answer;
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue
+            },
+        };
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
